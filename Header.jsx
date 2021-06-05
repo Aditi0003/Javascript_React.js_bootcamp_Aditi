0fda22aef2726dab6a90077731c65ea6eb83039e
Aditi0003 <85410516+Aditi0003@users.noreply.github.com>
2021-06-05 23:28:15 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+   return (
+      <header>
+              <h1> Hi! THIS  IS ADITI </h1>
+      </header>)
+};
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+   return (
+      <div className = "note">
+              <h1>Learning Javascript and React.Js </h1>
+              <p> 
+                This was really an amazing bootcamp by Shaurya Sinha.
+                I had a very good experience of learning and I got a 
+                basic idea about javascript and React.js, HTML. I would want
+                more such bootcamps in future as they are really useful in learning something 
+                from scratch.
+              </p>
+      </div>)
+};
+
+export default Note;
